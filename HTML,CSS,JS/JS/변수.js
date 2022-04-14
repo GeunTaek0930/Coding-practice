633b7e8a06928af2f8cdbf1136fd6b453cc0d412
GeunTaek0930 <shawn09300@gmail.com>
2022-04-14 21:17:18 +0900

    2022-04-14 js조건문

---
+++ HTML,CSS,JS/JS/변수.js
@@ -0,0 +1,17 @@
+let a = 2;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
+/* 값 재할당 불가, let =가능 const =불가능
+const a = 12;
+console.log(a);
+
+a = 999;
+console.log(a)
+*/
+
