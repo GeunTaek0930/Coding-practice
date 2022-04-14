6c720c1257387eea3e5b2419e94bef606a27f09a
GeunTaek0930 <shawn09300@gmail.com>
2022-04-14 21:17:52 +0900

    2022-04-14 js 데이터타입

---
+++ HTML,CSS,JS/JS/DataType.js
@@ -0,0 +1,37 @@
+let myName = "GEUNTAEK";
+let email = 'shawn0930@naver.com'
+let hello = `Hello ${myName}?!`
+let number = 123;
+let opacity = 1.57;
+let checked = true;
+let isShow = false;
+let undef;
+let obj = {
+  abc: 123
+};
+let empty = null;
+let user = {
+  name: `GEUNTAEK`,
+  age: 26,
+  isValid: true
+};
+let fruits = ['Apple', 'Banana', 'Cherry'];
+
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+console.log(number);
+console.log(opacity);
+console.log(checked);
+console.log(isShow);
+console.log(undef);
+console.log(obj.abc);
+console.log(obj.xyz);
+console.log(empty);
+console.log(user.name);
+console.log(user.age);
+console.log(user.isValid);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
